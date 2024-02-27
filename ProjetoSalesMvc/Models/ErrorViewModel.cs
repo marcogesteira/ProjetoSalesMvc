cb454e90e4ce3c6ee0b923bea0f5eb1233ed7418
Marco Gesteira <mgesteira@outlook.com>
2024-02-26 21:24:59 -0300

    Add project files.

---
+++ ProjetoSalesMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetoSalesMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
